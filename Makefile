daa589002e6e45b3fb2a1798eceabef2499e81b7
Mauri <maurito11@gmail.com>
2019-09-08 11:52:10 -0300

    dependencia boost removida...

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ clean:
 
 .PHONY:all
 all:
-	-g++  tp1.cpp -o tp1 ./libboost_program_options.a 
+	-g++  tp1.cpp -o tp1 -O1 -Wall -pedantic 
--- parser.argumentos.hpp
+++ parser.argumentos.hpp
@@ -1,33 +1,37 @@
-#include <boost/program_options.hpp>
-namespace po = boost::program_options;
-
 #include <iostream>
-#include <iterator>
 #include <tuple>
+#include <regex>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+
 using namespace std;
 
-std::tuple<uint32_t, uint32_t, char>  parse_command_line(int ac, char* av[]) {
+std::tuple<uint32_t, uint32_t, char> tuple_from_string(const string & str) {
+  regex r(R"(--N\s*(\d+)\s*--M\s*(\d+)\s*--S\s*(p|d){1})");
+  std::smatch sm;
+  regex_search(str, sm, r);
+  if ( sm.ready() && sm.size() == 4 ) {
+    string N = sm[1];
+    string M = sm[2];
+    string S = sm[3];
+
+    return make_tuple(static_cast<uint32_t>(stoul(N)),
+                      static_cast<uint32_t>(stoul(M)),
+                      S[0]);
+  }
+
+  throw std::runtime_error("Parametros (y orden) esperados: --N (num) --M (num) --S (p/d)"); 
+}
+
+std::tuple<uint32_t, uint32_t, char> parse_command_line(int ac, char* av[]) {
     try {
-
-        po::options_description desc("Parametros Esperados");
-        desc.add_options()
-            ("N", po::value<uint32_t>(), "Numero de Pacientes (n)")
-            ("M", po::value<uint32_t>(), "Numero de Donantes (m)")
-            ("S", po::value<char>()->default_value('p'), "p: pacientes - d: donantes")
-        ;
-
-        po::variables_map vm;        
-        po::store(po::parse_command_line(ac, av, desc), vm);
-        po::notify(vm);    
-
-        // Se retorna (N,M,S), p. ej.: (10,30,p)
-        return std::make_tuple(
-            vm["N"].as<uint32_t>(), 
-            vm["M"].as<uint32_t>(),
-            vm["S"].as<char>() == 'd' ? 'd' : 'p'
-        );
-
-
+      ostringstream oss{"",ios::ate};
+      for(size_t i=1; i < ac; ++i) {
+        oss << av[i] << ' ' ;
+      }
+      string str_parameters = oss.str();
+      return tuple_from_string(str_parameters);
     }
     catch(exception& e) {
         cerr << "error: " << e.what() << "\n";
